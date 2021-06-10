05e4e5591a290d11b3aae9634da3935a1fdd585c
Sean Ng <seanngpengnam@gmail.com>
2021-06-10 06:54:35 +0000

    Update

--- test/something.d
+++ test/something.d
@@ -1,15 +1,15 @@
 fbt:netlink:netlink_send:entry
-/arg0 == NULL/
+/arg2 == NULL/
 {
         stack();
 	printf("m is null in netlink");
 }
 
 fbt:netlink:netlink_send:entry
-/arg0 != NULL/
+/arg2 != NULL/
 {
         stack();
-	this->m = (struct mbuf*) arg0;
+	this->m = (struct mbuf*) arg2;
 	printf("m_len: %d\n", this->m->m_len);
 	printf("length long: %d\n", sizeof(long));
 }
